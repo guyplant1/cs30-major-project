58cce2755dc8a1d61ff9c9dbf740167557193c64
guyplant1 <guyplant1@hotmail.com>
2024-11-20 11:08:12 -0600

    Just added some testing code using orbit control and a box for a 3D environment.

--- sketch.js
+++ sketch.js
@@ -5,13 +5,33 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let boxX = 10;
+let boxY = 10;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  debugMode();
 }
 
 function draw() {
   background(220);
 
-  circle(mouseX, mouseY, 100);
+  //circle(mouseX, mouseY, 100);
+  orbitControl();
+  box(50, 50, 50, boxX, boxY);
+}
+
+function mousePressed() {
+  if (key === "w") {
+    boxY += 1;
+  }
+  if (key === "a") {
+    boxX -= 1;
+  }
+  if (key === "s") {
+    boxY -= 1;
+  }
+  if (key === "d") {
+    boxX += 1;
+  }
 }
