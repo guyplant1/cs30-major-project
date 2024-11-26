ea65286e0d711ffa51fb68feed64915827046faf
guyplant1 <guyplant1@hotmail.com>
2024-11-26 11:14:16 -0600

    Today I've added an array that makes a floor in the 3D game scene, and where I tried looking at previous code and implementing some of it into my project to generate different colors for each cell of the grid, to also see how each cell would look in their display, but it's still a work in progress to see each cell display. I'm planning to have the game scene be mainly a floor and possibly walls, and or the background display (an image and or color for the background).

--- sketch.js
+++ sketch.js
@@ -5,25 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let boxX = 10;
 const BOX_Y = 0;
+const CYL_Y = 0;
+let boxX = 10;
 let boxZ = 10;
 let cylX = 100;
-const CYL_Y = 0;
 let cylZ = 100;
+let gameGrid;
+let cellSize;
+let colorState = "red";
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   debugMode();
+  cellSize = height/12;
+  gameGrid = generateGrid(boxX, BOX_Y);
 }
 
 function draw() {
   background(220);
-
   //circle(mouseX, mouseY, 100);
   orbitControl();
-  playerDisplay();
-  gameBorder();
+  //playerDisplay();
+  //gameBorder();
+  //sphereDisplay();
+  displayGrid();
 }
 
 function keyPressed() {
@@ -48,7 +54,73 @@ function playerDisplay() {
 }
 
 function gameBorder() {
+  push();
   translate(cylX, CYL_Y, cylZ);
   fill("orange");
   box();
+  pop();
+}
+
+function sphereDisplay() {
+  push();
+  translate(-50, 0, -100);
+  fill('red');
+  sphere(50);
+  pop();
+
+  push();
+  translate(0, -100, -300);
+  fill('green');
+  sphere(50);
+  pop();
+
+  push();
+  translate(25, 25, 50);
+  fill('blue');
+  sphere(50);
+  pop();
+}
+
+function generateGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      // if (random(100) < 50) {
+      //   newGrid[y].push(0);
+      // }
+      // else {
+      //   newGrid[y].push(1);
+      // }
+      //newGrid[y].push(Math.floor(random(255)));
+    }
+  }
+  return newGrid;
+}
+
+function displayGrid() {
+  //let color = 0;
+  for (let y = 0; y < 5; y++) {
+    for (let x = 0; x < 5; x++) {
+      // if (gameGrid[y][x] === 0) {
+      //   fill("black");
+      // }
+      // else if (gameGrid[y][x] === 1) {
+      //   fill("white");
+      // }
+      //fill(random(255), random(255), random(255));
+      //fill(random(255));
+      // if (colorState === "red") {
+      //   fill(colorState);
+      //   box(boxX * cellSize, BOX_Y * cellSize, boxZ * cellSize);
+      //   colorState = "green";
+      // }
+      // else if (colorState === "green") {
+      //   fill(colorState);
+      //   box(boxX * cellSize, BOX_Y * cellSize, boxZ * cellSize);
+      //   colorState = "red";
+      // }
+      fill("orange");
+      box(boxX * cellSize, BOX_Y * cellSize, boxZ * cellSize);
+    }
+  }
 }
